1d80e64ecdf5dec50b12b35219829ebe3a77e1c9
Ben Toews <mastahyeti@users.noreply.github.com>
2017-02-09 11:09:39 -0700

    first commit

---
+++ SwiftKeys/SwiftKeys.h
@@ -0,0 +1,19 @@
+//
+//  SwiftKeys.h
+//  SwiftKeys
+//
+//  Created by Benjamin P Toews on 2/9/17.
+//  Copyright © 2017 GitHub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftKeys.
+FOUNDATION_EXPORT double SwiftKeysVersionNumber;
+
+//! Project version string for SwiftKeys.
+FOUNDATION_EXPORT const unsigned char SwiftKeysVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKeys/PublicHeader.h>
+
+
